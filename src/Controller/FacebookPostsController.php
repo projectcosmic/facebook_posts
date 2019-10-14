2aa63fac8e8cf8e237fdf2b0d772d73e8e8a56ca
Justin Wong <justin@cosmic.org.uk>
2019-10-14 10:27:01 +0100

    Fix retry link

--- src/Controller/FacebookPostsController.php
+++ src/Controller/FacebookPostsController.php
@@ -166,10 +166,11 @@ class FacebookPostsController {
       }
     }
     catch (\Exception $error) {
-      $retry = Url::fromRoute('facebook_posts.authenticate')->toString();
       $page['preface']['#markup'] = '<p>' . $this->t('We were unable to sufficiently authenticate with Facebook:') . '</p>';
       $page['error']['#markup'] = '<p>' . $error->getMessage() . '</p>';
-      $page['retry']['#markup'] = '<p>' . $this->t('<a href=":url">Try again</a>.', [':uri' => $retry]) . '</p>';
+      
+      $retry = Url::fromRoute('facebook_posts.authenticate')->toString();
+      $page['retry']['#markup'] = '<p>' . $this->t('<a href=":url">Try again</a>.', [':url' => $retry]) . '</p>';
     }
 
     return $page;
